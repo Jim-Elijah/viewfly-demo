01bcf2164882f6a6f01e08105037ea27562ce8bd
ustc-q <55303382+ustc-q@users.noreply.github.com>
2023-07-24 00:01:18 +0800

    feat: modify README; remove tabPositionSelector

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Tabs from './components/Tabs';
 import StartUp from './01-startup';
 import Basic from './02-basic';
 import Lifecycle from './03-lifecycle';
-import { tabItemList, tabPostionList } from './constant';
+import { tabItemList } from './constant';
 import "./index.less"
 
 const activeKey = tabItemList[0].key
@@ -29,21 +29,11 @@ const onTabClick = (key, e) => {
 }
 
 function App() {
-  const tabPosition = useSignal(tabPostionList[1])
-  const setTabPosition = (e) => {
-    console.log('setTabPosition', e.target.id);
-    tabPosition.set(e.target.id)
-  }
   return () => {
     return <>
       <div class="viewfly-header header">This is a demo for learning viewfly</div>
-      <div class="tab-position-selector"> tabPosition:
-        {
-          tabPostionList.map(item => <span class={{ 'selected': tabPosition() === item }} key={item} id={item} onClick={setTabPosition}>{item}</span>)
-        }
-      </div>
       <main>
-        <Tabs items={tabItems} defaultActiveKey={activeKey} tabPosition={tabPosition()} onChange={onTabChange} onTabClick={onTabClick}></Tabs>
+        <Tabs items={tabItems} defaultActiveKey={activeKey} tabPosition="left" onChange={onTabChange} onTabClick={onTabClick}></Tabs>
       </main>
       <div class="viewfly-footer">
         <div class="copyright">Copyright@{new Date().getFullYear()}
